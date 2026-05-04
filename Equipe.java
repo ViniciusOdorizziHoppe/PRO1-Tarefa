174184082f1cef1b34d26ec6393a9afa3157be10
Vinicius Odorizzi Hoppe <viniciusodorizzi25@gmail.com>
2026-05-04 09:26:08 -0300

    Create Equipe.java

---
+++ Equipe.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.usuario;
+
+/**
+ *
+ * @author aluno
+ */
+public class Equipe {
+    
+    private int identificadorDaEquipe;
+    private String nome;
+    private String tipo;
+    private String status;
+
+    public int getIdentificadorDaEquipe() {
+        return identificadorDaEquipe;
+    }
+
+    public void setIdentificadorDaEquipe(int identificadorDaEquipe) {
+        this.identificadorDaEquipe = identificadorDaEquipe;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    
+    
+}
