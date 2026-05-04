4cbec91d6667f438a60b81d1dfa773f3af024abf
Vinicius Odorizzi Hoppe <viniciusodorizzi25@gmail.com>
2026-05-04 09:24:25 -0300

    Create Patente class with getters and setters

---
+++ Patente.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.usuario;
+
+/**
+ *
+ * @author aluno
+ */
+public class Patente {
+    
+    private int identificadorDePatente;
+    private String nome;
+    private String descricao;
+    private String nivelHierarquico;
+
+    public int getIdentificadorDePatente() {
+        return identificadorDePatente;
+    }
+
+    public void setIdentificadorDePatente(int identificadorDePatente) {
+        this.identificadorDePatente = identificadorDePatente;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getNivelHierarquico() {
+        return nivelHierarquico;
+    }
+
+    public void setNivelHierarquico(String nivelHierarquico) {
+        this.nivelHierarquico = nivelHierarquico;
+    }
+    
+    
+}
